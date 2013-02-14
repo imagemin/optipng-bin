69fbd9d22827f370fe1d67ded892d920e7708356
badunk <baduncaduncan@gmail.com>
2013-02-14 15:38:54 -0800

    removing optipng output files, modified script to be using its own exported path module

--- rebuild-optipng.js
+++ rebuild-optipng.js
@@ -1,15 +1,16 @@
 var exec = require('child_process').exec;
 var colors = require('colors');
+var path = require('./lib/optipng-bin.js').path;
 
 if (process.platform === 'darwin' || process.platform === 'linux') {
     exec('make clean && ./configure && make && make test', {cwd: './optipng/', stdio: 'inherit'}, function(error, stdout, stderr) {
-        exec('cp optipng/src/optipng/optipng vendor/osx/', function(err){
+        exec('cp optipng/src/optipng/optipng ' + path, function(err){
             if (err) {
                 console.log(err.red);
                 return;
             }
 
-            console.log('OptiPNG rebuilt successfully for OSX'.green);
+            console.log('OptiPNG rebuilt successfully'.green);
         });
     });
 }
