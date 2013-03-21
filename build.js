e08db6c7b193c9bbd9b7762780ee0902aa78e0f0
badunk <baduncaduncan@gmail.com>
2013-03-20 23:49:15 -0700

    doing prebuild test before deciding necessary to rebuild optipng

--- build.js
+++ build.js
@@ -6,22 +6,24 @@ var which = require('which');
 var path = require('path');
 
 
-which('make', function (err) {
-    if (err) {
-        return console.log(err.red);
-    }
+module.exports = function(){
+    which('make', function (err) {
+        if (err) {
+            return console.log(err.red);
+        }
 
-    if (process.platform === 'darwin' || process.platform === 'linux') {
-        var binDir = path.dirname(binPath);
-        var buildScript = 'make clean &&' +
-                          './configure --with-system-zlib --bindir=' + binDir  + ' --mandir=man && ' +
-                          'make install';
-        exec(buildScript, { cwd: './optipng/' }, function (err) {
-            if (err) {
-                return console.log(err.red);
-            }
+        if (process.platform === 'darwin' || process.platform === 'linux') {
+            var binDir = path.dirname(binPath);
+            var buildScript = 'make clean &&' +
+                              './configure --with-system-zlib --bindir=' + binDir  + ' --mandir=man && ' +
+                              'make install';
+            exec(buildScript, { cwd: './optipng/' }, function (err) {
+                if (err) {
+                    return console.log(err.red);
+                }
 
-            console.log('OptiPNG rebuilt successfully'.green);
-        });
-    }
-});
+                console.log('OptiPNG rebuilt successfully'.green);
+            });
+        }
+    });
+}
---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+var Mocha = require('mocha');
+var mocha = new Mocha({ui: 'bdd', reporter: 'list'});
+var build = require('./build.js');
+var colors = require('colors');
+
+mocha.addFile('test/test-optipng-path.js');
+
+var runner = mocha.run(function(failures){
+    if (failures > 0){
+        build();
+    } else{
+        console.log('pre-build test passed successfully, skipping build'.green);
+    }
+});
+
+
