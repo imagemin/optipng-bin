987ce48ab8e9f12a5739cc6df7ec285a83757039
Sindre Sorhus <sindresorhus@gmail.com>
2013-04-08 18:32:53 +0200

    Minor style tweaks and tabs

--- pre-build.js
+++ pre-build.js
@@ -1,17 +1,15 @@
 'use strict';
 var Mocha = require('mocha');
-var mocha = new Mocha({ui: 'bdd', reporter: 'list'});
-var build = require('./build.js');
 var colors = require('colors');
+var build = require('./build.js');
+var mocha = new Mocha({ui: 'bdd', reporter: 'list'});
 
 mocha.addFile('test/test-optipng-path.js');
 
-var runner = mocha.run(function (failures) {
-    if (failures > 0){
-        build();
-    } else{
-        console.log('pre-build test passed successfully, skipping build'.green);
-    }
+mocha.run(function (failures) {
+	if (failures > 0) {
+		build();
+	} else {
+		console.log('pre-build test passed successfully, skipping build'.green);
+	}
 });
-
-
