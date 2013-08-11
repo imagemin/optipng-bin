17cecadc6d51bf8a1e739cac6cfae9e37161cb3d
Sindre Sorhus <sindresorhus@gmail.com>
2013-08-12 01:50:03 +0200

    Minor tweaks

--- test/test-build.js
+++ test/test-build.js
@@ -7,8 +7,10 @@ var binPath = require('../lib/optipng.js').path;
 
 describe('OptiPNG rebuild', function () {
 	it('it should rebuild the optipng binaries', function (cb) {
-		this.timeout(15000); // give this test time to build
+		this.timeout(15000);
+
 		var origCTime = fs.statSync(binPath).ctime;
+
 		exec('node build.js', {}, function (err) {
 			var actualCTime = fs.statSync(binPath).ctime;
 			assert(actualCTime !== origCTime);
