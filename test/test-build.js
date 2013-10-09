acb7b832fef0ea4d80277044623fe2623e43a681
Kevin Martensson <kevinmartensson@gmail.com>
2013-10-09 10:22:12 +0200

    `bin.path` should always return local binary in tests

--- test/test-build.js
+++ test/test-build.js
@@ -10,12 +10,11 @@ var path = require('path');
 describe('optipng.build()', function () {
 	it('should rebuild the optipng binaries', function (cb) {
 		this.timeout(false);
-
-		options.path = path.join(__dirname, '../tmp');
-		options.buildScript = './configure --with-system-zlib --bindir=' + path.join(__dirname, '../tmp') +
-				 			  ' --mandir=man && make install';
-
 		var bin = new Bin(options);
+
+		bin.path = path.join(__dirname, '../tmp', bin.bin);
+		bin.buildScript = './configure --with-system-zlib --bindir=' + path.join(__dirname, '../tmp') + ' && ' +
+				 		  'make install';
 
 		bin.build(function () {
 			var origCTime = fs.statSync(bin.path).ctime;
