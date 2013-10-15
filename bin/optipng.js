52f3ec543f873c0fa9d96f498219dc2bdf718a44
Kevin Martensson <kevinmartensson@gmail.com>
2013-10-15 08:35:42 +0200

    Remove redundant `console.log()`

--- bin/optipng.js
+++ bin/optipng.js
@@ -4,7 +4,5 @@
 var binPath = require('../lib/optipng').path;
 var spawn = require('child_process').spawn;
 
-console.log(binPath);
-
 spawn(binPath, process.argv.slice(2), { stdio: 'inherit' })
 	.on('exit', process.exit);
